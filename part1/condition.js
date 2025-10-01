acac3bd05be3ace56dbc89fa18026224b601c8a5
Chandan-Yadav03 <chandankr2944@gmail.com>
2025-10-01 20:21:17 +0530

    learn non primitive datatype and condition

---
+++ part1/condition.js
@@ -0,0 +1,17 @@
+// let a=10;
+// let b=20;
+// if(a>b)
+// {
+//     console.log("a is greater than b");
+// }
+// else{
+//     console.log("b is greater than a");
+// }
+let score=50;
+if(typeof score ==='number')
+{
+    console.log("Score is a number");
+}
+else{
+    console.log("Score is not a number");
+}
---
+++ part1/nonPrimitive.js
@@ -0,0 +1,9 @@
+const userName={
+    firstName: "Chandan",
+    "last Name":"Yadav",
+    age : 25,
+};
+console.log(userName);
+console.log(userName.firstName);
+console.log(userName["last Name"]);
+console.log(typeof(userName));
