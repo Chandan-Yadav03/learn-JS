45773b72d17c97b865be5acb3d17ce07d99a7052
Chandan-Yadav03 <chandankr2944@gmail.com>
2025-10-02 15:12:15 +0530

    learn arrays

---
+++ part1/array.js
@@ -0,0 +1,18 @@
+// let teaFlavors= ["green tea", "black tea", "oolong tea"];
+// let firstTea= teaFlavors[0];
+// console.log(firstTea);
+
+//question 2
+let cities= ["London", "Tokyo", "Paris", "New York"];
+let thirdCity= cities[2];
+let hardcopyCities=[...cities];
+let softcopyCities=cities;
+// cities.push("Berlin");
+// console.log(cities);
+// console.log(softcopyCities);
+
+// console.log(hardcopyCities);
+
+let cityBucketList= ["Kyoto", "London", "Cape Town"];
+let isLondonList = cityBucketList.includes("London");
+console.log(isLondonList); // true
