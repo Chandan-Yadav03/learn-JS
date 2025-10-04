69977d64fbf4ca2eff1ae0b351d360b204546e07
Chandan-Yadav03 <chandankr2944@gmail.com>
2025-10-04 15:27:34 +0530

    learn export and import

---
+++ part4_adv/importCommon.js
@@ -0,0 +1,4 @@
+const mathOperation= require("./common.js");
+
+console.log(mathOperation.add(2,2));
+console.log(mathOperation.sub(40,6));
---
+++ part4_adv/importModule.js
@@ -0,0 +1,6 @@
+import mul from "./module.js";
+import {add,sub} from "./module.js";
+
+console.log("Addition ="+ add(2,2));
+console.log("Subtraction=" + sub(5,2));
+console.log("Multiplication=" + mul(2,7));
